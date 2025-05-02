1c14970dba474d224fb9eb0c3a78511c4103a1db
Aman Kumar <authorisedaman@gmail.com>
2025-05-02 13:49:57 +0530

    adede

--- src/pages/WorkPage.tsx
+++ src/pages/WorkPage.tsx
@@ -25,6 +25,44 @@ const WorkPage = () => {
         from="April 2025"
         to="Present"
       />
+
+      <Work
+        name="DecorTwist"
+        live="https://decortwist.com/"
+        skills={[
+          "Shopify",
+          "Shiprocket",
+          "Wati",
+          "Google Docs",
+          "Google Sheets",
+          "MS Word",
+          "MS Excel",
+          "MS Powerpoint",
+        ]}
+        description="As a business analyst in a startup, I analyzed data, created Excel reports, managed customer feedback, and communicated insights with clients and teams. I also prepared presentations and reports in PowerPoint and Word to drive improvements."
+        post="Business Analyst"
+        from="May 2022"
+        to="September 2022"
+      />
+
+      <Work
+        name="Aimil ltd."
+        live="https://www.aimil.com/"
+        skills={[
+          "VibXpert ||",
+          "VibXpert |||",
+          "PRUFTECNIK App",
+          "Vibration Analysis",
+          "Alignment Analysis",
+          "MS Word",
+          "MS Excel",
+          "MS Powerpoint",
+        ]}
+        description="I collected and analyzed vibration and alignment data using VibXpert II, VibXpert III, and the PRUFTECNIK App. I conducted vibration and alignment assessments, and created detailed reports using MS Word, Excel, and PowerPoint to optimize machine performance."
+        post="Service Engineer"
+        from="July 2021"
+        to="May 2022"
+      />
     </motion.div>
   );
 };
