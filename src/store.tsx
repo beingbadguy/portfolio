f475c1aaee2ae679bc3cb300d97b2bd294c62797
Aman Kumar <authorisedaman@gmail.com>
2025-05-16 13:53:06 +0530

    added ip

--- src/store.tsx
+++ src/store.tsx
@@ -84,6 +84,7 @@ export const useAudioStore = create<AudioState>((set, get) => ({
       const res = await fetch("https://api64.ipify.org?format=json");
       const data = await res.json();
       const ip = data.ip;
+      console.log(ip);
 
       // Store in Supabase
       const { data: existing } = await supabase
